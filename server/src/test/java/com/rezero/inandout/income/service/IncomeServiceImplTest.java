e6ab50ef44f6056c03455288abf49a87417caa47
jiyongYoon <jyyoon0615@naver.com>
2022-10-19 01:27:12 +0900

    FEAT/Income 등록 테스트

---
+++ server/src/test/java/com/rezero/inandout/income/service/IncomeServiceImplTest.java
@@ -0,0 +1,127 @@
+package com.rezero.inandout.income.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.rezero.inandout.income.entity.DetailIncomeCategory;
+import com.rezero.inandout.income.model.IncomeInput;
+import com.rezero.inandout.income.repository.DetailIncomeCategoryRepository;
+import com.rezero.inandout.income.repository.IncomeRepository;
+import com.rezero.inandout.member.entity.Member;
+import com.rezero.inandout.member.repository.MemberRepository;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class IncomeServiceImplTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private IncomeRepository incomeRepository;
+
+    @Mock
+    private DetailIncomeCategoryRepository detailIncomeCategoryRepository;
+
+    @InjectMocks
+    private IncomeServiceImpl incomeService;
+
+    @Nested
+    @DisplayName("수입내역 추가")
+    class addIncomeMethod {
+
+        Member member = Member.builder()
+            .memberId(10L)
+            .build();
+
+        IncomeInput incomeInput1 = IncomeInput.builder()
+            .member(member)
+            .incomeDt(LocalDate.now().minusMonths(1))
+            .incomeItem("초콜릿")
+            .detailIncomeCategoryId(100L)
+            .incomeAmount(2000)
+            .incomeMemo("income1-memo")
+            .build();
+
+        IncomeInput incomeInput2 = IncomeInput.builder()
+            .member(member)
+            .incomeDt(LocalDate.now())
+            .incomeItem("새우깡")
+            .detailIncomeCategoryId(100L)
+            .incomeAmount(3500)
+            .incomeMemo("income2-memo")
+            .build();
+
+        DetailIncomeCategory detailIncomeCategory = DetailIncomeCategory.builder()
+            .detailIncomeCategoryId(99L)
+            .detailIncomeCategoryName("간식")
+            .build();
+        List<IncomeInput> incomes;
+
+
+        @Test
+        @DisplayName("수입내역 추가 - 성공")
+        void addIncome_success() {
+            //given
+            given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.of(member));
+
+            given(detailIncomeCategoryRepository.findByDetailIncomeCategoryId(any()))
+                .willReturn(Optional.of(detailIncomeCategory));
+
+            incomes = new ArrayList<>();
+            incomes.add(incomeInput1);
+            incomes.add(incomeInput2);
+
+            //when
+            String email = "test@email.com";
+            incomeService.addIncome(email, incomes);
+
+            //then
+            verify(incomeRepository, times(1)).saveAll(any());
+        }
+
+        @Test
+        @DisplayName("수입내역 추가 실패 - member 없음")
+        void addIncome_fail_no_member() {
+            //given
+            given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.empty());
+
+            //when
+            String email = "test@email.com";
+
+            //then
+            assertThrows(RuntimeException.class, () -> incomeService.addIncome(email, incomes));
+        }
+
+        @Test
+        @DisplayName("수입내역 추가 실패 - category 없음")
+        void addIncome_fail_no_category() {
+            //given
+            given(memberRepository.findByEmail(any()))
+                .willReturn(Optional.of(member));
+
+            //when
+            String email = "test@email.com";
+
+            //then
+            assertThrows(RuntimeException.class, () -> incomeService.addIncome(email, incomes));
+        }
+
+    }
+
+}
