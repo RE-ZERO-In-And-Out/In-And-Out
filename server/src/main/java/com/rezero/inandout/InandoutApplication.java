0c5b6817ba70b77afbfc4b222a0dc912961da3f5
Ran <lan4250@naver.com>
2022-10-20 01:19:46 +0900

    FEAT/ InandoutApplication 클래스에password encoder 빈 등록

--- server/src/main/java/com/rezero/inandout/InandoutApplication.java
+++ server/src/main/java/com/rezero/inandout/InandoutApplication.java
@@ -2,12 +2,19 @@ package com.rezero.inandout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class InandoutApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InandoutApplication.class, args);
-	}
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(InandoutApplication.class, args);
+    }
 
 }
