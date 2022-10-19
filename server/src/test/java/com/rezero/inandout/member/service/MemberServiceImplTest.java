9b329a04c30c83471fcbf9869d1aaecbe22d3106
Ran <lan4250@naver.com>
2022-10-20 01:30:37 +0900

    TEST/ 회원 아이디(이메일), 비밀번호 찾기

--- server/src/test/java/com/rezero/inandout/member/service/MemberServiceImplTest.java
+++ server/src/test/java/com/rezero/inandout/member/service/MemberServiceImplTest.java
@@ -10,6 +10,7 @@ import com.rezero.inandout.member.model.JoinMemberInput;
 import com.rezero.inandout.member.repository.MemberRepository;
 import java.time.LocalDate;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -60,5 +61,57 @@ class MemberServiceImplTest {
         // then
         verify(memberRepository, times(1)).save(member);
 
+    }
+
+    @Test
+    @DisplayName("아이디 찾기 - 이메일 확인")
+    void findEmail() {
+
+        // given
+        Member member = Member.builder()
+            .email("egg@naver.com")
+            .address("서울특별시")
+            .phone("010-2222-0000")
+            .birth(LocalDate.from(LocalDate.of(2000, 9, 30)))
+            .gender("남")
+            .nickName("원빈")
+            .password("abc!@#12")
+            .build();
+
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.of(member));
+
+        // when
+        String email = "egg@naver.com";
+        String findEmail = memberService.findEmail(email);
+
+        // then
+        Assertions.assertEquals(email, findEmail);
+
+    }
+
+    @Test
+    @DisplayName("비밀번호 찾기 - 이메일, 휴대폰 일치 확인")
+    void findPhone() {
+
+        // given
+        Member member = Member.builder()
+            .email("egg@naver.com")
+            .address("서울특별시")
+            .phone("010-2222-0000")
+            .birth(LocalDate.from(LocalDate.of(2000, 9, 30)))
+            .gender("남")
+            .nickName("강동원")
+            .password("abc!@#12")
+            .build();
+
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.of(member));
+
+        // when
+        String email = "egg@naver.com";
+        String phone = "010-2222-0000";
+        String findPhone = memberService.findPhone(email, phone);
+
+        // then
+        Assertions.assertEquals(findPhone, phone);
     }
 }
