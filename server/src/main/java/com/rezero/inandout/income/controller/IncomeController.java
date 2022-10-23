5c7bd4bf60dd70a08b2b1ee8da007351cb49a7d9
jiyongYoon <jyyoon0615@naver.com>
2022-10-23 23:22:01 +0900

    FEAT /IncomeController Swagger 세팅

--- server/src/main/java/com/rezero/inandout/income/controller/IncomeController.java
+++ server/src/main/java/com/rezero/inandout/income/controller/IncomeController.java
@@ -7,6 +7,8 @@ import com.rezero.inandout.income.model.IncomeCategoryDto;
 import com.rezero.inandout.income.model.IncomeDto;
 import com.rezero.inandout.income.model.IncomeInput;
 import com.rezero.inandout.income.service.IncomeServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -30,6 +32,7 @@ public class IncomeController {
 
     private final IncomeServiceImpl incomeService;
 
+    @ApiOperation(value = "수입 등록 API", notes = "수입내용을 입력하면 저장됩니다.")
     @PostMapping
     public ResponseEntity<?> addIncome(Principal principal,
                                         @RequestBody @Validated List<IncomeInput> incomeInputList) {
@@ -50,10 +53,13 @@ public class IncomeController {
         return ResponseEntity.ok().body("저장에 성공했습니다.");
     }
 
+    @ApiOperation(value = "수입 조회 API", notes = "조회할 기간을 입력하면 해당하는 수입내역이 조회됩니다.")
     @GetMapping
     public ResponseEntity<?> getIncomeListAndDetailCategoryList(Principal principal,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDt,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDt) {
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @ApiParam(value = "조회할 기간의 시작일", example = "2022-10-01") LocalDate startDt,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @ApiParam(value = "조회할 기간의 마지막일", example = "2022-10-31") LocalDate endDt) {
 
         CategoryAndIncomeDto categoryAndIncomeDto = CategoryAndIncomeDto.builder()
             .incomeDtoList(incomeService.getIncomeList(principal.getName(), startDt, endDt))
@@ -64,6 +70,7 @@ public class IncomeController {
     }
 
 
+    @ApiOperation(value = "수입 삭제 API", notes = "삭제할 내역의 아이디값을 입력하면 해당 내역이 삭제됩니다.")
     @DeleteMapping
     public ResponseEntity<?> deleteIncome(Principal principal,
                              @RequestBody List<DeleteIncomeInput> deleteIncomeInputList) {
