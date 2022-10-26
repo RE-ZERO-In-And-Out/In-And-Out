5a83d12f12298e5a03f9bd82b4a3c2451a15cc93
Ran <lan4250@naver.com>
2022-10-27 01:36:44 +0900

    FEAT/ 회원 로그아웃 관련 ErrorCode 추가 및 ErrorCode 이름 변경

--- server/src/main/java/com/rezero/inandout/exception/errorcode/MemberErrorCode.java
+++ server/src/main/java/com/rezero/inandout/exception/errorcode/MemberErrorCode.java
@@ -10,9 +10,9 @@ public enum MemberErrorCode {
     /**
      * 회원가입 - 이미 존재하는 이메일, 휴대폰, 닉네임 오류
      */
-    EXIST_EMAIL("이미 가입된 이메일입니다."),
-    EXIST_PHONE("동일한 휴대폰 번호가 존재합니다. 다른 번호를 입력하세요."),
-    EXIST_NICKNAME("동일한 닉네임이 존재합니다. 다른 닉네임을 입력하세요"),
+    EMAIL_EXIST("이미 가입된 이메일입니다."),
+    PHONE_EXIST("동일한 휴대폰 번호가 존재합니다. 다른 번호를 입력하세요."),
+    NICKNAME_EXIST("동일한 닉네임이 존재합니다. 다른 닉네임을 입력하세요"),
 
     /**
      * 회원가입 - 비밀번호 오류
@@ -28,9 +28,10 @@ public enum MemberErrorCode {
     /**
      * 회원 조회 오류
      */
-    NOT_EXIST_MEMBER("회원 정보가 존재하지 않습니다."),
-    NOT_EXIST_EMAIL("이메일이 존재하지 않습니다."),
-    NOT_EXIST_PHONE("연락처(번호)가 존재하지 않습니다."),
+    MEMBER_NOT_EXIST("회원 정보가 존재하지 않습니다."),
+    EMAIL_NOT_EXIST("이메일이 존재하지 않습니다."),
+    PHONE_NOT_EXIST("연락처(번호)가 존재하지 않습니다."),
+    CANNOT_GET_INFO("현재 로그인 상태가 아니라서 회원 정보를 조회할 수 없습니다."),
 
     /**
      * 회원 정보 수정 오류
@@ -40,12 +41,12 @@ public enum MemberErrorCode {
     /**
      * 비밀번호 오류
      */
-    NOT_MATCH_PASSWORD("회원 비밀번호를 잘못 입력했습니다."),
+    PASSWORD_NOT_MATCH("회원 비밀번호를 잘못 입력했습니다."),
 
     /**
      * 로그아웃 오류
      */
-    NOT_LOGGED_IN("현재 로그인 상태가 아니어서 로그아읏 할 수 없습니다");
+    CANNOT_LOGOUT("현재 로그인 상태가 아니어서 로그아웃 할 수 없습니다");
 
 
     private final String description;
