c23f2f3d315c53e8209ad58a8d9ddf53ca86d7f2
jiyongYoon <jyyoon0615@naver.com>
2022-10-26 21:48:23 +0900

    FIX / Controller test 수정 - @MockBean(JpaMetamodelMappingContext.class) 붙임

--- server/src/test/java/com/rezero/inandout/income/controller/IncomeControllerTest.java
+++ server/src/test/java/com/rezero/inandout/income/controller/IncomeControllerTest.java
@@ -31,12 +31,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.test.web.servlet.MockMvc;
 
+@MockBean(JpaMetamodelMappingContext.class)
 @WebMvcTest(IncomeController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @DisplayName("IncomeController 테스트")
