73f5a295d548cbf8714f1ef4b27cb8079ee803fb
jiyongYoon <jyyoon0615@naver.com>
2022-10-29 01:06:50 +0900

    REFACTOR/report_month_income 구조 및 메서드 위치 수정 테스트코드 작성

--- server/src/test/java/com/rezero/inandout/income/repository/IncomeQueryRepositoryTest.java
+++ server/src/test/java/com/rezero/inandout/income/repository/IncomeQueryRepositoryTest.java
@@ -41,6 +41,126 @@ class IncomeQueryRepositoryTest {
         QIncome income = new QIncome("i");
         QIncomeCategory incomeCategory = new QIncomeCategory("ic");
 
+        List<ReportDto> reportDtoList = new ArrayList<>();
+
+        ReportDto reportDto1 = ReportDto.builder()
+            .category("주수입")
+            .categorySum(1234567)
+            .categoryRatio(80.0)
+            .build();
+
+        ReportDto reportDto2 = ReportDto.builder()
+            .category("부수입")
+            .categorySum(12345)
+            .categoryRatio(20.0)
+            .build();
+
+        reportDtoList.add(reportDto1);
+        reportDtoList.add(reportDto2);
+
+        JPAQuery step1 = mock(JPAQuery.class);
+        given(queryFactory
+            .select(Projections.constructor(ReportDto.class,
+            incomeCategory.incomeCategoryName,
+            income.incomeAmount.sum(),
+            income.incomeAmount.sum()
+                .multiply(100)
+                .doubleValue())
+            )
+        ).willReturn(step1);
+
+        JPAQuery step2 = mock(JPAQuery.class);
+        given(step1.from(any(EntityPath.class)))
+            .willReturn(step2);
+
+        JPAQuery step3 = mock(JPAQuery.class);
+        given(step2.leftJoin(any(EntityPath.class), any(EntityPath.class)))
+            .willReturn(step3);
+
+        JPAQuery step4 = mock(JPAQuery.class);
+        given(step3.where(any(BooleanExpression.class)))
+            .willReturn(step4);
+
+        JPAQuery step5 = mock(JPAQuery.class);
+        given(step4.groupBy(incomeCategory.incomeCategoryName))
+            .willReturn(step5);
+
+        JPAQuery step6 = mock(JPAQuery.class);
+        given(step5.orderBy(income.incomeAmount.sum().desc()))
+            .willReturn(step6);
+
+        mock(JPAQuery.class);
+        given(step6.fetch())
+            .willReturn(reportDtoList);
+
+        //when
+        List<ReportDto> queryReportDtoList
+            = incomeQueryRepository.getMonthlyIncomeReport(1L,
+            LocalDate.of(2022,10,1),
+            LocalDate.of(2022,10,31)
+        );
+
+        //then
+        assertEquals(queryReportDtoList.get(0).getCategory(), "주수입");
+        assertEquals(queryReportDtoList.get(0).getCategorySum(), 1234567);
+        assertEquals(queryReportDtoList.get(0).getCategoryRatio(), 80.0);
+        assertEquals(queryReportDtoList.get(1).getCategory(), "부수입");
+        assertEquals(queryReportDtoList.get(1).getCategorySum(), 12345);
+        assertEquals(queryReportDtoList.get(1).getCategoryRatio(), 20.0);
+        assertEquals(queryReportDtoList.size(), 2);
+    }
+
+    @Test
+    @DisplayName("월 수입 보고서에 필요한 합계 조회 - 0")
+    void getMonthlyIncomeSum_success_0() {
+        Integer intA = 0;
+
+        List<ReportDto> reportDtoList = new ArrayList<>();
+
+        ReportDto reportDto1 = ReportDto.builder()
+            .category("주수입")
+            .categorySum(1234567)
+            .categoryRatio(80.0)
+            .build();
+
+        ReportDto reportDto2 = ReportDto.builder()
+            .category("부수입")
+            .categorySum(12345)
+            .categoryRatio(20.0)
+            .build();
+
+        reportDtoList.add(reportDto1);
+        reportDtoList.add(reportDto2);
+
+        JPAQuery step1 = mock(JPAQuery.class);
+        given(queryFactory.select(any(NumberExpression.class)))
+            .willReturn(step1);
+
+        JPAQuery step2 = mock(JPAQuery.class);
+        given(step1.from(any(EntityPath.class)))
+            .willReturn(step2);
+
+        JPAQuery step3 = mock(JPAQuery.class);
+        given(step2.where(
+            any(BooleanExpression.class)))
+            .willReturn(step3);
+
+        given(step3.fetchOne())
+            .willReturn(intA);
+
+        //when
+        int sum0
+            = incomeQueryRepository.getMonthlyIncomeSum(1L,
+            LocalDate.of(2022,10,1),
+            LocalDate.of(2022,10,31));
+
+        //then
+        assertEquals(sum0, 0);
+    }
+
+    @Test
+    @DisplayName("월 수입 보고서에 필요한 합계 조회 - int")
+    void getMonthlyIncomeSum_success_int() {
         Integer intA = 10000;
 
         List<ReportDto> reportDtoList = new ArrayList<>();
@@ -76,61 +196,13 @@ class IncomeQueryRepositoryTest {
         given(step3.fetchOne())
             .willReturn(intA);
 
-        JPAQuery step5 = mock(JPAQuery.class);
-        given(queryFactory
-            .select(Projections.constructor(ReportDto.class,
-            incomeCategory.incomeCategoryName,
-            income.incomeAmount.sum(),
-            income.incomeAmount.sum()
-                .multiply(100)
-                .doubleValue()
-                .divide(intA)
-                .multiply(100)
-                .round()
-                .divide(100.0)
-                )
-            )
-        ).willReturn(step5);
-
-        JPAQuery step6 = mock(JPAQuery.class);
-        given(step5.from(any(EntityPath.class)))
-            .willReturn(step6);
-
-        JPAQuery step7 = mock(JPAQuery.class);
-        given(step6.leftJoin(any(EntityPath.class), any(EntityPath.class)))
-            .willReturn(step7);
-
-        JPAQuery step8 = mock(JPAQuery.class);
-        given(step7.where(any(BooleanExpression.class)))
-            .willReturn(step8);
-
-        JPAQuery step9 = mock(JPAQuery.class);
-        given(step8.groupBy(incomeCategory.incomeCategoryName))
-            .willReturn(step9);
-
-        JPAQuery step10 = mock(JPAQuery.class);
-        given(step9.orderBy(income.incomeAmount.sum().desc()))
-            .willReturn(step10);
-
-        mock(JPAQuery.class);
-        given(step10.fetch())
-            .willReturn(reportDtoList);
-
-
         //when
-        List<ReportDto> queryReportDtoList
-            = incomeQueryRepository.getMonthlyIncomeReport(1L,
+        int sum
+            = incomeQueryRepository.getMonthlyIncomeSum(1L,
             LocalDate.of(2022,10,1),
-            LocalDate.of(2022,10,31)
-        );
+            LocalDate.of(2022,10,31));
 
         //then
-        assertEquals(queryReportDtoList.get(0).getCategory(), "주수입");
-        assertEquals(queryReportDtoList.get(0).getCategorySum(), 1234567);
-        assertEquals(queryReportDtoList.get(0).getCategoryRatio(), 80.0);
-        assertEquals(queryReportDtoList.get(1).getCategory(), "부수입");
-        assertEquals(queryReportDtoList.get(1).getCategorySum(), 12345);
-        assertEquals(queryReportDtoList.get(1).getCategoryRatio(), 20.0);
-        assertEquals(queryReportDtoList.size(), 2);
+        assertEquals(sum, 10000);
     }
 }
