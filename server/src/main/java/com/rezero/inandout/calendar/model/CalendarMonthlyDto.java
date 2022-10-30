20f378e277e86ef2b41ae81343117c45d48dca07
jiyongYoon <jyyoon0615@naver.com>
2022-10-31 03:25:22 +0900

    - 달력 조회 API Dto 구성 - Diary Dto는 프론트단에서 개별 API로 호출하도록 할 예정 -> get과 post가 함께 진행되기 때문

---
+++ server/src/main/java/com/rezero/inandout/calendar/model/CalendarMonthlyDto.java
@@ -0,0 +1,22 @@
+package com.rezero.inandout.calendar.model;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CalendarMonthlyDto {
+    int year;
+    int month;
+
+    List<CalendarIncomeDto> calendarIncomeDtoList;
+    List<CalendarExpenseDto> calendarExpenseDtoList;
+
+}
