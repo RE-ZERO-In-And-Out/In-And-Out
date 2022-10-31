35f638ab3854a5acce0efc67a0073b2d2c4b3edd
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2022-10-31 19:35:46 +0900

    FEAT/Diary 등록 테스트코드

--- server/src/test/java/com/rezero/inandout/diary/controller/DiaryControllerTest.java
+++ server/src/test/java/com/rezero/inandout/diary/controller/DiaryControllerTest.java
@@ -7,24 +7,31 @@ import com.rezero.inandout.member.entity.Member;
 import com.rezero.inandout.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -65,21 +72,21 @@ class DiaryControllerTest {
                         .nickName("홍길동")
                         .diaryDt(LocalDate.of(2020, 10, 1))
                         .text("굿")
-                        .diaryPhotoUrl("/diary/photo/~~")
+//                        .diaryPhotoUrl("/diary/photo/~~")
                         .build(),
                 DiaryDto.builder()
                         .diaryId(2L)
                         .nickName("홍길동")
                         .diaryDt(LocalDate.of(2020, 10, 2))
                         .text("굿")
-                        .diaryPhotoUrl("/diary/photo/~~")
+//                        .diaryPhotoUrl("/diary/photo/~~")
                         .build(),
                 DiaryDto.builder()
                         .diaryId(3L)
                         .nickName("홍길동")
                         .diaryDt(LocalDate.of(2020, 10, 3))
                         .text("굿")
-                        .diaryPhotoUrl("/diary/photo/~~")
+//                        .diaryPhotoUrl("/diary/photo/~~")
                         .build()
         );
         given(diaryService.getDiaryList(any(), any(), any()))
@@ -96,5 +103,46 @@ class DiaryControllerTest {
                 .andExpect(jsonPath("$.[0].diaryId").value(1L))
                 .andExpect(jsonPath("$.[1].diaryId").value(2L))
                 .andExpect(jsonPath("$.[2].diaryId").value(3L));
+    }
+
+    @Test
+    @DisplayName("일기 저장 테스트")
+    void addDiary() throws Exception {
+        //given
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("hgd@gmail.com")
+                .password("1234")
+                .build();
+
+        User user = new User(member.getEmail(), member.getPassword(), AuthorityUtils.NO_AUTHORITIES);
+        TestingAuthenticationToken testingAuthenticationToken = new TestingAuthenticationToken(user,null);
+
+        MockMultipartFile file1 = new MockMultipartFile("file",
+                "test.png",
+                "image/png",
+                "«‹png data>>".getBytes());
+
+        MockMultipartFile file2 = new MockMultipartFile("addDiaryInput",
+                "", "application/json",
+                ("{\"diaryDt\" : \"2022-10-01\"," + " \"text\" : \"강아지 귀엽다 ㅎㅎ\"}")
+                        .getBytes(StandardCharsets.UTF_8));
+
+        //when
+        mockMvc.perform(
+                multipart("/api/diary")
+                        .file(file1)
+                        .file(file2)
+                        .principal(testingAuthenticationToken)
+        ).andExpect(status().isOk())
+                .andDo(print());
+
+        ArgumentCaptor<LocalDate> captorDiaryDt = ArgumentCaptor.forClass(LocalDate.class);
+        ArgumentCaptor<String> captorText = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<MultipartFile> captor2 = ArgumentCaptor.forClass(MultipartFile.class);
+        //then
+        verify(diaryService, times(1))
+                .addDiary(any(), captorDiaryDt.capture(),
+                        captorText.capture(), captor2.capture());
     }
 }
--- server/src/test/java/com/rezero/inandout/diary/service/impl/DiaryServiceImplTest.java
+++ server/src/test/java/com/rezero/inandout/diary/service/impl/DiaryServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.rezero.inandout.diary.service.impl;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.rezero.inandout.diary.entity.Diary;
 import com.rezero.inandout.diary.model.DiaryDto;
 import com.rezero.inandout.diary.repository.DiaryRepository;
+import com.rezero.inandout.exception.DiaryException;
 import com.rezero.inandout.exception.MemberException;
 import com.rezero.inandout.exception.errorcode.MemberErrorCode;
 import com.rezero.inandout.member.entity.Member;
@@ -11,20 +14,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("DiaryServiceImpl 테스트")
@@ -36,8 +42,15 @@ class DiaryServiceImplTest {
     @Mock
     private MemberRepository memberRepository;
 
+    @Mock
+    private AmazonS3Client amazonS3Client;
+
     @InjectMocks
     private DiaryServiceImpl diaryServiceImpl;
+
+    @Test
+    void addDiary() {
+    }
 
     @Nested
     @DisplayName("일기 목록 조회")
@@ -54,21 +67,21 @@ class DiaryServiceImplTest {
                         .member(member)
                         .diaryDt(LocalDate.of(2020, 10, 1))
                         .text("굿")
-                        .diaryPhotoUrl("/diary/photo/~~")
+//                        .diaryPhotoUrl("/diary/photo/~~")
                         .build(),
                 Diary.builder()
                         .diaryId(2L)
                         .member(member)
                         .diaryDt(LocalDate.of(2020, 10, 2))
                         .text("굿")
-                        .diaryPhotoUrl("/diary/photo/~~")
+//                        .diaryPhotoUrl("/diary/photo/~~")
                         .build(),
                 Diary.builder()
                         .diaryId(3L)
                         .member(member)
                         .diaryDt(LocalDate.of(2020, 10, 3))
                         .text("굿")
-                        .diaryPhotoUrl("/diary/photo/~~")
+//                        .diaryPhotoUrl("/diary/photo/~~")
                         .build()
         );
 
@@ -106,6 +119,79 @@ class DiaryServiceImplTest {
 
             //then
             assertEquals(MemberErrorCode.MEMBER_NOT_EXIST, exception.getErrorCode());
+        }
+    }
+
+    @Nested
+    @DisplayName("일기 등록")
+    class addDiary {
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("hgd@gmail.com")
+                .password("1234")
+                .build();
+
+        PutObjectResult result = new PutObjectResult();
+
+        MockMultipartFile file = new MockMultipartFile("file",
+                "test.png",
+                "image/png",
+                "«‹png data>>".getBytes());
+
+        @Test
+        @DisplayName("일기 등록 - 성공")
+        void addDiary_success() {
+            //given
+            given(memberRepository.findByEmail(anyString()))
+                    .willReturn(Optional.of(member));
+
+            given(amazonS3Client.putObject(any()))
+                    .willReturn(result);
+            //when
+            diaryServiceImpl.addDiary("hgd@gmail.com",
+                    LocalDate.of(2022,10,1),
+                    "굿", file);
+
+            //then
+            verify(diaryRepository, times(1)).save(any());
+        }
+
+        @Test
+        @DisplayName("일기 등록 - 실패 : 멤버 없음")
+        void addDiary_fail_memberNotExist() {
+            //given
+            given(memberRepository.findByEmail(anyString()))
+                    .willReturn(Optional.empty());
+
+            //when
+            MemberException exception = assertThrows(MemberException.class,
+                    () -> diaryServiceImpl.addDiary("hgd@gmail.com",
+                            LocalDate.of(2022,10,1),
+                            "굿", file)
+                    );
+
+            //then
+            assertEquals(MemberErrorCode.MEMBER_NOT_EXIST, exception.getErrorCode());
+        }
+
+        @Test
+        @DisplayName("일기 등록 - 실패 : 확인되지 않은 익셉션")
+        void addDiary_fail_no() {
+            //given
+            given(memberRepository.findByEmail(anyString()))
+                    .willReturn(Optional.of(member));
+
+            given(amazonS3Client.putObject(any()))
+                    .willThrow(new DiaryException("??"));
+            //when
+            DiaryException exception = assertThrows(DiaryException.class,
+                    () -> diaryServiceImpl.addDiary("hgd@gmail.com",
+                            LocalDate.of(2022,10,1),
+                            "굿", file)
+            );
+
+            //then
+            assertEquals("??", exception.getMessage());
         }
     }
 }
