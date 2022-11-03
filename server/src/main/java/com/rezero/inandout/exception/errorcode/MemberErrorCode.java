3b7d9653cbbc645dc86de1729dd4c39a98820f27
Ran <lan4250@naver.com>
2022-11-03 12:12:20 +0900

    FEAT / 회원 상태에 따른 에러 코드 추가, 에러 코드 이름 변경

--- server/src/main/java/com/rezero/inandout/exception/errorcode/MemberErrorCode.java
+++ server/src/main/java/com/rezero/inandout/exception/errorcode/MemberErrorCode.java
@@ -8,12 +8,13 @@ import lombok.Getter;
 public enum MemberErrorCode {
 
     /**
-     * 회원가입 - 이미 존재하는 이메일, 휴대폰, 닉네임 오류, 탈퇴 회원
+     * 회원가입 - 이미 존재하는 이메일, 휴대폰, 닉네임 오류, 탈퇴 / 정지 회원
      */
-    EMAIL_EXIST("이미 가입된 이메일입니다."),
+    EMAIL_EXIST("이미 가입된 이메일입니다. 가입된 이메일로 로그인 하시길 바랍니다."),
     PHONE_EXIST("동일한 휴대폰 번호가 존재합니다. 다른 번호를 입력하세요."),
     NICKNAME_EXIST("동일한 닉네임이 존재합니다. 다른 닉네임을 입력하세요"),
-    WITHDRAWAL_MEMBER("이미 탈퇴한 아이디(이메일)입니다. 다른 이메일로 회원 가입하시길 바랍니다."),
+    WITHDRAWAL_MEMBER_CANNOT_JOIN("이미 탈퇴한 아이디(이메일)입니다. 다른 이메일로 회원 가입하시길 바랍니다."),
+    STOP_MEMBER_CANNOT_JOIN("정지된 회원입니다. 관리자에게 문의 바랍니다."),
 
     /**
      * 회원가입 - 비밀번호 오류
@@ -27,9 +28,10 @@ public enum MemberErrorCode {
     PASSWORD_NOT_CONTAIN_CHARACTER_AND_SPECIAL("비밀번호는 영문자, 특수 문자를 각각 한 글자 이상 포함해야합니다."),
 
     /**
-     * 회원 가입 - 이메일 인증 오류
+     * 회원 가입 - 이메일 인증 오류 (가입 요청 중인 회원)
      */
     EMAIL_AUTH_KEY_NOT_EXIST("이메일 인증키가 존재하지 않습니다."),
+    REQ_MEMBER_CANNOT_JOIN("현재 회원가입 요청 중입니다. 이메일 인증을 하시고 나면 회원 가입이 완료됩니다."),
 
     /**
      * 이메일 발송 오류
@@ -61,9 +63,9 @@ public enum MemberErrorCode {
     /**
      * 로그인 오류
      */
-    CANNOT_LOGIN_REQ("이메일 인증을 완료해야 로그인이 가능합니다."),
-    CANNOT_LOGIN_STOP("정지된 회원입니다. 관리자에게 문의하시길 바랍니다."),
-    CANNOT_LOGIN_WITHDRAW("이미 탈퇴한 회원입니다. 새로운 아이디로 회원가입 후, 로그인하시길 바랍니다."),
+    REQ_MEMBER_CANNOT_LOGIN("이메일 인증을 완료해야 로그인이 가능합니다."),
+    STOP_MEMBER_CANNOT_LOGIN("정지된 회원입니다. 관리자에게 문의하시길 바랍니다."),
+    WITHDRAWAL_MEMBER_CANNOT_LOGIN("이미 탈퇴한 회원입니다. 새로운 아이디로 회원가입 후, 로그인하시길 바랍니다."),
 
     /**
      * 로그아웃 오류
