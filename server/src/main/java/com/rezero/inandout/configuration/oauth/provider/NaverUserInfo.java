45b200bb63ddee73673eeb18f070576df3ae5d86
Ran <lan4250@naver.com>
2022-11-08 16:15:46 +0900

    FIX/ OAuth 회원 정보 중 email을 oauthUsername로 변경

--- server/src/main/java/com/rezero/inandout/configuration/oauth/provider/NaverUserInfo.java
+++ server/src/main/java/com/rezero/inandout/configuration/oauth/provider/NaverUserInfo.java
@@ -21,11 +21,6 @@ public class NaverUserInfo implements OAuth2UserInfo {
     }
 
     @Override
-    public String getEmail() {
-        return (String) attributes.get("email");
-    }
-
-    @Override
     public String getProvider() {
         return "naver";
     }
--- server/src/main/java/com/rezero/inandout/configuration/oauth/provider/OAuth2UserInfo.java
+++ server/src/main/java/com/rezero/inandout/configuration/oauth/provider/OAuth2UserInfo.java
@@ -3,6 +3,5 @@ package com.rezero.inandout.configuration.oauth.provider;
 public interface OAuth2UserInfo {
     String getProviderId();
     String getProvider();
-    String getEmail();
     String getName();
 }
