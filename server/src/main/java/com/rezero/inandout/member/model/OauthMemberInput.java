82ef20a79b254084de7dd5b121168ec790834f2d
Ran <lan4250@naver.com>
2022-11-10 00:42:54 +0900

    FIX/ OauthMemberInput 에 대해 @NotBlank 제거

--- server/src/main/java/com/rezero/inandout/member/model/OauthMemberInput.java
+++ server/src/main/java/com/rezero/inandout/member/model/OauthMemberInput.java
@@ -1,6 +1,5 @@
 package com.rezero.inandout.member.model;
 
-import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,19 +14,14 @@ import lombok.Setter;
 @AllArgsConstructor
 public class OauthMemberInput {
 
-    @NotBlank(message = " 'provider_providerId'의 형태로 저장해야합니다.")
     private String oauthUsername;
 
-    @NotBlank(message = "닉네임을 정확하게 입력하세요.")
     private String nickName;
 
-    @NotBlank(message = "연락처를 정확하게 입력하세요.")
     private String phone;
 
-    @NotBlank(message = "provider를 정확하게 입력하세요.")
     private String provider;
 
-    @NotBlank(message = "providerId를 정확하게 입력하세요.")
     private String providerId;
 
 }
