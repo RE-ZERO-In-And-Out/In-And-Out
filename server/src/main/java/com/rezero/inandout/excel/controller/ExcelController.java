3602d5090e3be7fac4f76105074887b013db7117
jiyongYoon <jyyoon0615@naver.com>
2022-11-09 22:00:06 +0900

    - ExcelController 매핑 변경

--- server/src/main/java/com/rezero/inandout/excel/controller/ExcelController.java
+++ server/src/main/java/com/rezero/inandout/excel/controller/ExcelController.java
@@ -16,18 +16,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/excel")
 public class ExcelController {
 
     private final ExcelServiceImpl excelService;
 
     @ApiOperation(value = "수입내역 Excel 출력 API",
         notes = "조회할 기간을 입력하면 해당하는 수입내역이 엑셀로 출력됩니다.")
-    @GetMapping("/excel/income")
+    @GetMapping("/income")
     public ResponseEntity<InputStreamResource> downloadIncomeExcelFile(
                     HttpServletRequest request, Principal principal,
                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -41,7 +43,7 @@ public class ExcelController {
 
     @ApiOperation(value = "지출내역 Excel 출력 API",
         notes = "조회할 기간을 입력하면 해당하는 지출내역이 엑셀로 출력됩니다.")
-    @GetMapping("/excel/expense")
+    @GetMapping("/expense")
     public ResponseEntity<InputStreamResource> downloadExpenseExcelFile(
                     HttpServletRequest request, Principal principal,
                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -56,7 +58,7 @@ public class ExcelController {
     @ApiOperation(value = "연간보고서 Excel 출력 API",
         notes = "조회할 시작일 입력한 후 연간보고서 페이지의 데이터를 Json 형태로 Post 하면 "
             + "시작일 기준 12개월의 연간보고서가 엑셀로 출력됩니다.")
-    @PostMapping("/excel/year")
+    @PostMapping("/year")
     public ResponseEntity<InputStreamResource> downloadYearlyReportExcelFile(
             HttpServletRequest request, Principal principal,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
