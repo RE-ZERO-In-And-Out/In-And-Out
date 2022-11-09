e9a5a44bd0be1327da58b15a40991d5e594140ee
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2022-11-09 23:07:45 +0900

    FIX/LocalDate @PastOrPresent 삭제, @NotNull 추가

--- server/src/main/java/com/rezero/inandout/diary/model/UpdateDiaryInput.java
+++ server/src/main/java/com/rezero/inandout/diary/model/UpdateDiaryInput.java
@@ -16,7 +16,7 @@ public class UpdateDiaryInput {
     @NotNull(message = "일기아이디를 입력하세요.")
     @Min(value = 1, message = "일기아이디는 1이상입니다.")
     private Long diaryId;
-    @PastOrPresent(message = "일기작성일(과거 또는 현재)을 입력하세요.")
+    @NotNull(message = "일기작성일을 입력하세요.")
     private LocalDate diaryDt;
 
     private String text;
