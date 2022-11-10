f0662d467b7e1615a13620767588e18a7e941237
Ran <lan4250@naver.com>
2022-11-11 02:37:49 +0900

    REFACTOR/ properties 파일에 변수 redirect url 생성

--- server/src/main/java/com/rezero/inandout/configuration/UserAuthenticationSuccessHandler.java
+++ server/src/main/java/com/rezero/inandout/configuration/UserAuthenticationSuccessHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
@@ -16,12 +17,15 @@ public class UserAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
 
     private final MemberRepository memberRepository;
 
+    @Value(value = "${url.after.login}")
+    private String urlAfterLogin;
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException, ServletException {
 
         String oauthUsername = authentication.getName();
-        String frontLoginUrl = "http://3.34.206.181:3000/calendar";
 
 
         /*
@@ -36,7 +40,7 @@ public class UserAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
         */
 
         log.info("[Member Authentication] OAuth member: " + oauthUsername);
-        setDefaultTargetUrl(frontLoginUrl);
+        setDefaultTargetUrl(urlAfterLogin);
         super.onAuthenticationSuccess(request, response, authentication);
 
     }
